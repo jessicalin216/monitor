1107e95513a9dc476c3c72f7aec147e2e0e81c06
angie <t-anng@microsoft.com>
2016-07-27 02:23:36 -0700

    new shit homies sorry

---
+++ app/src/main/java/com/microsoft/band/monitor/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.microsoft.band.monitor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+}
