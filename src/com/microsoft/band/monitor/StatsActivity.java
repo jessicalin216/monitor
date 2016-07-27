6e6368ccd6e999d9c0b137c4d7a135b265738374
Vivian Zhang <vzhang0722@gmail.com>
2016-07-27 00:19:01 -0700

    alternating row colors merged

--- src/com/microsoft/band/monitor/StatsActivity.java
+++ src/com/microsoft/band/monitor/StatsActivity.java
@@ -1,14 +1,18 @@
 package com.microsoft.band.monitor;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TabHost;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
 public class StatsActivity extends Activity {
     TabHost tabHost;
-    ListView listView;
+    ListView dates;
+    ListView duration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +34,55 @@ public class StatsActivity extends Activity {
         host.addTab(spec);
 
         // List View
-        listView = (ListView) findViewById(R.id.list);
+        dates = (ListView) findViewById(R.id.dates);
+        duration = (ListView) findViewById(R.id.duration);
+
         String[] blah = new String[]{"I'm on my period", "Such bad cramps", "I want chocolate", "I'm sad"};
+        String[] plah = new String[]{"1","2","3","4"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, blah);
+        ArrayAdapter<String> datesAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, blah){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the current item from ListView
+                View view = super.getView(position,convertView,parent);
+                if(position %2 == 1)
+                {
+                    // Set a background color for ListView regular row/item
+                    view.setBackgroundColor(Color.parseColor("#843c96"));
+                }
+                else
+                {
+                    // Set the background color for alternate row/item
+                    view.setBackgroundColor(Color.parseColor("#C8A2C8"));
+                }
+                return view;
+            }
+        };
 
-        listView.setAdapter(adapter);
+        ArrayAdapter<String> durationAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, plah){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the current item from ListView
+                View view = super.getView(position,convertView,parent);
+                if(position %2 == 1)
+                {
+                    // Set a background color for ListView regular row/item
+                    view.setBackgroundColor(Color.parseColor("#843c96"));
+
+                }
+                else
+                {
+                    // Set the background color for alternate row/item
+                    view.setBackgroundColor(Color.parseColor("#C8A2C8"));
+                }
+                return view;
+            }
+        };
+
+        dates.setAdapter(datesAdapter);
+        duration.setAdapter(durationAdapter);
 
     }
 }
